633492deec0001b468f2c3f46fe8aca36d54e101
qinjiawei <738437340@qq.com>
2022-06-06 22:01:36 +0800

    update

--- src/main/scala/com/dahuatech/scala/Demo.scala
+++ src/main/scala/com/dahuatech/scala/Demo.scala
@@ -4,25 +4,8 @@ import scala.util.control.Breaks
 
 object Demo {
   def main(args: Array[String]): Unit = {
-    def a(s: String) = {
-      println(s)
-    }
 
-    println(a _)
-    a("alan")
 
-    val b = a _
-    println(b)
-    b("jack")
 
-    val f = (s: String) => println(s)
-    println(f)
-    f("asdfa")
-
-    def aa(a: Int)(b: Int) = {
-      a + b
-    }
-
-    println(aa(10)(20))
   }
 }
